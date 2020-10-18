46b0a55b9dac504dcee59b693fb1f7226d8bf085
Orn Reaksmeyarun <reaksmeyarunorn@gmail.com>
2020-10-18 20:36:06 +0700

    favorite, detail, cart, each Item activity

---
+++ app/src/main/java/com/reaksmeyarun/pda/view/activity/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.reaksmeyarun.pda.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.reaksmeyarun.pda.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
